74a1255423b1af7affe13d93cfe8558f32ccf16c
James Clark <jamesloganclark@gmail.com>
2019-08-13 11:17:22 -0700

    Adds a Login and Sign Up page
    I haven't included any validation yet for emails and passwords, this is just the framework

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { theme } from "./themes/theme";
 import LandingPage from "./pages/Landing";
+import LoginPage from "./pages/Login";
+import SignUpPage from "./pages/Signup";
 
 import "./App.css";
 
@@ -11,7 +13,10 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <Switch>
+          <Route exact path="/" component={LoginPage} />
+          <Route path="/signup" component={SignUpPage} />
+        </Switch>
       </BrowserRouter>
     </MuiThemeProvider>
   );
